da7bc489ea6c761acfa42945c542d83b5da71a2f
avelyy <dldkwls3221@naver.com>
2022-04-13 16:44:39 +0900

    typeScript 및 vuex, vue router 적용

---
+++ src/main/frontend/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is Login Vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main/frontend/vue.config.js
+++ src/main/frontend/vue.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  transpileDependencies: true,
   //outputDir : npm run build로 빌드 시에 파일이 생성되는 위치
   outputDir: "../resources/static",
-  transpileDependencies: true
+  devServer: {
+    proxy: {
+      '/login': {target: 'http://localhost:8082'},
+      '/api': {target: 'http://localhost:8082'}
+    }
+  }
+
 })
